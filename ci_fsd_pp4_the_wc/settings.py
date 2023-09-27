9f69a66cf5285e1e0259caf8d79624e2a95b7e07
Parbelaez <132989779+Parbelaez@users.noreply.github.com>
2023-09-27 11:38:47 +0200

    Debug set to false

--- ci_fsd_pp4_the_wc/settings.py
+++ ci_fsd_pp4_the_wc/settings.py
@@ -34,7 +34,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1','thewcwebpage-83a6428384c3.herokuapp.com']
 
