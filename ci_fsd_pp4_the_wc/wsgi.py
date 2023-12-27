c4abddfdfcb1b9360edcd4231495d52346df7690
Parbelaez <132989779+Parbelaez@users.noreply.github.com>
2023-12-27 10:03:38 +0100

    New setup for static files, plus debug flag

--- ci_fsd_pp4_the_wc/wsgi.py
+++ ci_fsd_pp4_the_wc/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for ci_fsd_pp4_the_wc project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
